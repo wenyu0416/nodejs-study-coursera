6006c9de87925b5d1eaaec84e77cdb0cc006d6d8
wenyu0416 <wenyu0416@163.com>
2021-10-06 17:50:26 +0800

    change to callback version, timeout simulation

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var rect = require('./rectangle');
 function solveRect(l, b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greateer than zero: l = " + l + ", and b = " + b);
-    } else {
-        console.log("The area of the rectange is " + rect.area(l, b));
-        console.log("The perimeter of the rectange is " + rect.perimeter(l, b));
-    }
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("Error:" + err.message);
+        } else {
+            console.log("The area of the rectange is " + rectangle.area);
+            console.log("The perimeter of the rectange is " + rectangle.perimeter);
+        }
+    });
 }
 
 solveRect(2, 4);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,10 @@
-exports.perimeter = (x, y) => (2 * (x + y));
-exports.area = (x, y) => (x * y);
+module.exports = (x, y, cb) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() => cb(new Error("Rectangle dimensions should be greateer than zero: l = " + x + ", and b = " + y), null), 2000);
+    } else {
+        setTimeout(() => cb(null, {
+            perimeter: (2 * (x + y)),
+            area: (x * y)
+        }), 2000)
+    }
+}
