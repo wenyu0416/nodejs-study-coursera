b5fa99dd8052e3f18903137d631ef0b6fb449396
wenyu0416 <wenyu0416@163.com>
2021-10-06 16:03:38 +0800

    replace the inline var with imported module

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 console.log("hello Node.js");
 
-var rect = {
-    perimeter: (x, y) => (2 * (x + y)),
-    area: (x, y) => (x * y)
-};
+import { area, perimeter } from './rectangle';
 
 function solveRect(l, b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
@@ -11,8 +8,8 @@ function solveRect(l, b) {
     if (l <= 0 || b <= 0) {
         console.log("Rectangle dimensions should be greateer than zero: l = " + l + ", and b = " + b);
     } else {
-        console.log("The area of the rectange is " + rect.area(l, b));
-        console.log("The perimeter of the rectange is " + rect.perimeter(l, b));
+        console.log("The area of the rectange is " + area(l, b));
+        console.log("The perimeter of the rectange is " + perimeter(l, b));
     }
 }
 
